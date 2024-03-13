57c462e368346577e5e813eb56d7d4c8ca392fa0
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 <luxlux124@gmail.com>
2024-03-13 11:05:14 +0300

    add app 'main' and set all allowed host

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gcetrvc@hq#@a9mi4i$p7)87pja2skcum-b5l*6!=!=$l*44c3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
