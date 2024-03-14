3e612c4b65746ae9d8eed459f5a6ba0b0c6fef31
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 <luxlux124@gmail.com>
2024-03-15 02:38:44 +0300

    tranfer query set to custom tag

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-from goods.models import Categories
-
 
 def index(request):
 
-    categories = Categories.objects.all()
     context: dict[str:any] = {
         'title': 'Home App',
         'content': 'Main page of shop',
-        'categories': categories
     }
     return render(request, 'main/index.html', context=context)
 
