e4b9376bb1387134dc702daf0da188f954b0a257
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 <luxlux124@gmail.com>
2024-03-16 04:02:32 +0300

    add search form

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import get_list_or_404, render
 from django.core.paginator import Paginator
 
+from goods.utils import q_search
 from goods.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     page = request.GET.get('page', 1)
-    goods = (
-        Products.objects.all()
-        if category_slug == 'all'
-        else get_list_or_404(Products.objects.filter(
-            category_id__slug=category_slug)
+
+    goods = None
+    if query := request.GET.get('q'):
+        goods = q_search(query)
+    else:
+        goods = (
+            Products.objects.all()
+            if category_slug == 'all'
+            else get_list_or_404(Products.objects.filter(
+                category_id__slug=category_slug)
+            )
         )
-    )
 
     if request.GET.get('on_sale', None):
         goods = goods.filter(discount__gt=0)
@@ -30,9 +36,9 @@ def catalog(request, category_slug):
     return render(request, 'goods/catalog.html', context)
 
 
-def product(request, product_slag=None, product_id=None):
-    kwargs = {'slug': product_slag} \
-        if product_slag  \
+def product(request, product_slug=None, product_id=None):
+    kwargs = {'slug': product_slug} \
+        if product_slug  \
         else {'id': product_id}
     product = Products.objects.get(**kwargs)
     context = {
