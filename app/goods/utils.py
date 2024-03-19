f688279b401042e69afde96e2731a22fee7a48f7
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 <luxlux124@gmail.com>
2024-03-20 00:31:59 +0300

    add SearchHeadline for text

--- app/goods/utils.py
+++ app/goods/utils.py
@@ -1,16 +1,34 @@
 from goods.models import Products
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-
-# from django.db.models import Q
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline
+)
 
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    vector = SearchVector("name", "description")
+    search_columns = ("name", "description")
+    vector = SearchVector(*search_columns)
     query = SearchQuery(query)
 
-    return Products.objects.annotate(
+    data = Products.objects.annotate(
         rank=SearchRank(vector, query)
     ).filter(rank__gt=0).order_by('-rank')
+
+    span_style = '"background-color: yellow;"'
+    for search_table in search_columns:
+        kwargs = {search_table + '_line':
+                  SearchHeadline(
+                      search_table,
+                      query,
+                      start_sel=f"<span style={span_style}>",
+                      stop_sel="</span>",
+                  )}
+
+        data = data.annotate(**kwargs)
+
+    return data
