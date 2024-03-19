ae91189adbf9c8e057fa96ad224d2eb20b7abba2
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 <luxlux124@gmail.com>
2024-03-19 22:12:44 +0300

    minor data changes

--- app/goods/models.py
+++ app/goods/models.py
@@ -31,7 +31,7 @@ class Products(models.Model):
         default='0.00', max_digits=4, decimal_places=2, verbose_name='Скидка%')
     quantity = models.PositiveIntegerField(
         default=0, verbose_name='Количество')
-    category_id = models.ForeignKey(to=Categories, on_delete=models.PROTECT)
+    category = models.ForeignKey(to=Categories, on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'product'
