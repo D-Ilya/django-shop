40dc5f379b27d644ac765e6606e0e6d61785668b
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 <luxlux124@gmail.com>
2024-03-19 22:13:39 +0300

    swithing to postgres

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_shop',  # имя вашей базы данных
+        'USER': 'avalon',  # ваш пользователь PostgreSQL
+        'PASSWORD': 'avalon',  # ваш пароль PostgreSQL
+        'HOST': '0.0.0.0',  # хост для подключения к контейнеру
+        'PORT': '5432',  # порт для подключения к контейнеру
     }
 }
 
