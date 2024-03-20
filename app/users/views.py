6963412a289973936d7ed9a7a73c6ee5a6389c49
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 <luxlux124@gmail.com>
2024-03-21 01:31:59 +0300

    create UserRegistrationForm

--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 
@@ -28,8 +28,18 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        if (form := UserRegistrationForm(data=request.POST)) and form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context = {
-        'title': 'Регистрация'
+        'title': 'Регистрация',
+        'form': form
     }
 
     return render(request, 'users/registration.html', context)
@@ -44,4 +54,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
