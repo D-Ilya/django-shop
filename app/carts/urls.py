403e61e10c0edaf101b50b733be11cb4f6699d72
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 <luxlux124@gmail.com>
2024-04-02 02:08:08 +0300

    developed func 'cart_add'

--- app/carts/urls.py
+++ app/carts/urls.py
@@ -4,10 +4,10 @@ from carts import views
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart_add/<int:product_id>/',
+    path('cart_add/<slug:product_slug>/',
          views.cart_add, name='cart_add'),
-    path('cart_cnange/<int:product_id>/',
+    path('cart_cnange/<slug:product_slug>/',
          views.cart_cnange, name='cart_cnange'),
-    path('cart_remove/<int:product_id>/',
+    path('cart_remove/<slug:product_slug>/',
          views.cart_remove, name='cart_remove'),
 ]
--- app/carts/views.py
+++ app/carts/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from carts.models import Cart
+from goods.models import Products
 
 # Create your views here.
 
 
-def card_add(request, product_id):
+def cart_add(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product)
+        if carts.exists():
+            cart = carts.first()
+            cart.quntity += 1
+            cart.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quntity=1)
+    else:
+        carts = Cart.objects.filter(
+            session_key=request.session.session_key, product=product)
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def cart_cnange(request, product_slug):
     ...
 
 
-def card_cnange(request, product_id):
-    ...
-
-
-def card_remove(request, product_id):
+def cart_remove(request, product_slug):
     ...
